3f4d50ffbbab1363e4644ce1dcdcab59a1708793
this.gerardochavez <info.gerardo.chavez@gmail.com>
2024-08-18 15:56:47 -0300

    adding goEvent BE

--- src/assets/components/main/Portfolio.jsx
+++ src/assets/components/main/Portfolio.jsx
@@ -1,49 +1,80 @@
-import React from 'react'
+import React from "react";
 
-import SectionTitles from '../support/modules/SectionTitles'
+import SectionTitles from "../support/modules/SectionTitles";
 
-import '../../styles/portfolio.css'
-import data from '../../components/support/data/PortfolioData.js'
+import "../../styles/portfolio.css";
+import data from "../../components/support/data/PortfolioData.js";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
-      <SectionTitles 
-        h5 = 'Portfolio'
-        h2 ='My Recent Work'
-      />
-      
+    <section id="portfolio">
+      <SectionTitles h5="Portfolio" h2="My Recent Work" />
+
       <div className="container portfolio__container">
-        {
-          data.map(({ id, image, title, technologies, github, demo }) => {
-            return(
-              <article key={id}className = 'portfolio__item'>
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} />
-    
-              </div>
-              <h3>{title}</h3>
-              <h5>Done with {technologies}</h5>
-              <div className="portfolio__item-cta">
-                <a href={github} className='btn'>Repository</a>
-                <a href={demo} className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
-              
-              </div>
-    
-            </article>
+        {data.map(
+          ({
+            id,
+            image,
+            title,
+            technologies,
+            github,
+            demo,
+            gitlab,
+            demoapi,
+          }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <h5>Done with {technologies}</h5>
+                <div className="portfolio__item-cta">
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Repository
+                  </a>
+                  <a
+                    href={demo}
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live Demo
+                  </a>
+                </div>
 
-           )
-          
-          })
-        
-        }
-
+                {gitlab && (
+                  <div className="portfolio__item-cta">
+                    <a
+                      href={gitlab}
+                      className="btn"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      BE Repository
+                    </a>
+                    <a
+                      href={demoapi}
+                      className="btn btn-primary"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live Demo
+                    </a>
+                  </div>
+                )}
+              </article>
+            );
+          }
+        )}
       </div>
-    
     </section>
-  
-  )
+  );
+};
 
-}
-
-export default Portfolio
+export default Portfolio;
--- src/assets/components/support/data/PortfolioData.js
+++ src/assets/components/support/data/PortfolioData.js
@@ -1,132 +1,128 @@
-import IMG1 from '../../../images/portfolio1.jpg'
-import IMG2 from '../../../images/portfolio2.jpg'
-import IMG3 from '../../../images/portfolio3.jpg'
-import IMG4 from '../../../images/portfolio4.jpg'
-import IMG5 from '../../../images/portfolio5.png'
-import IMG6 from '../../../images/portfolio6.jpg'
-import IMG7 from '../../../images/portfolio7.jpg'
-import IMG8 from '../../../images/portfolio8.png'
-import IMG9 from '../../../images/airport-location-2959.png'
-import IMG10 from '../../../images/product.png'
-import IMG11 from '../../../images/robot.png'
-import IMG12 from '../../../images/d395045c-b147-4d2d-92a9-603339fdb47b.jpg'
+import IMG1 from "../../../images/portfolio1.jpg";
+import IMG2 from "../../../images/portfolio2.jpg";
+import IMG3 from "../../../images/portfolio3.jpg";
+import IMG4 from "../../../images/portfolio4.jpg";
+import IMG5 from "../../../images/portfolio5.png";
+import IMG6 from "../../../images/portfolio6.jpg";
+import IMG7 from "../../../images/portfolio7.jpg";
+import IMG8 from "../../../images/portfolio8.png";
+import IMG9 from "../../../images/airport-location-2959.png";
+import IMG10 from "../../../images/product.png";
+import IMG11 from "../../../images/robot.png";
+import IMG12 from "../../../images/d395045c-b147-4d2d-92a9-603339fdb47b.jpg";
 
-const data=[
-    {
-        id : 13,
-        image: IMG12,
-        title: "goEvent App",
-        technologies: "React, NextJS, Clerk, Stripe, Uploadthing, deployed on Vercel",
-        github: "https://github.com/ijchavez/goevent_platform",
-        demo: "https://goevent-platform.vercel.app/"
-    },
-    {
-        id : 8,
-        image: IMG11,
-        title: 'Banking App',
-        technologies: 'React deployed on Vercel',
-        github: 'https://gitlab.com/ijchavez/bank_modern_app',
-        demo: 'https://bank-modern-app-zeta.vercel.app/'
-
-    },{
-        id : 11,
-        image: IMG8,
-        title: 'E-Commerce',
-        technologies: 'NextJS, React, Sanity, Stripe deployed on Vercel',
-        github: 'https://github.com/ijchavez/ecommerce-sanity-stripe-jsm',
-        demo: 'https://ecommerce-sanity-stripe-jsm.vercel.app/'
-
-    },
-    {
-        id : 1,
-        image: IMG1,
-        title: 'Notes APP',
-        technologies: 'NodeJS, MongoDB, bryptJS, connect-flash, express, express-handlebars, mongoose, passport deployed on Fly.io',
-        github: 'https://github.com/ijchavez/notes-app',
-        demo: 'https://new-notes-app.fly.dev/'
-
-    },
-    {
-        id : 9,
-        image: IMG9,
-        title: 'Airport Flight Information',
-        technologies: 'React - Axios deployed on Vercel',
-        github: 'https://gitlab.com/ijchavez/flight-information',
-        demo: 'https://flight-information.vercel.app/'
-
-    },
-    {
-        id : 2,
-        image: IMG2,
-        title: 'Budget',
-        technologies: 'Javascript',
-        github: 'https://github.com/ijchavez/JavaScript/tree/master/Leccion21_AplicacionPresupuesto',
-        demo: 'https://ijchavez.github.io/JavaScript/Leccion21_AplicacionPresupuesto/index.html'
-
-    },
-    {
-        id : 3,
-        image: IMG3,
-        title: 'To-Do List',
-        technologies: 'Javascript',
-        github: 'https://ijchavez.github.io/JavaScript/ToDoList/index.html',
-        demo: 'https://ijchavez.github.io/JavaScript/ToDoList/index.html'
-
-    },
-    {
-        id : 4,
-        image: IMG4,
-        title: 'Product API',
-        technologies: 'NodeJS, ExpressJS deployed on Render',
-        github: 'https://gitlab.com/ijchavez/nodejs',
-        demo: 'https://productsapi-n5p2.onrender.com/api/v1/products'
-
-    },
-    {
-        id : 10,
-        image: IMG10,
-        title: 'Product Catalog',
-        technologies: 'React - Axios, consuming data from NodeJS API, deployed on Vercel',
-        github: 'https://gitlab.com/ijchavez/productsapi',
-        demo: 'https://productsapi-six.vercel.app/'
-
-    },
-    {
-        id : 12,
-        image: IMG10,
-        title: 'Product Catalog',
-        technologies: 'AngularTS, consuming data from NodeJS API, deployed on Vercel',
-        github: 'https://bitbucket.org/LucasHood/angularfinalutn/',
-        demo: 'https://angularfinalutn.vercel.app/'
-
-    },{
-        id : 5,
-        image: IMG5,
-        title: 'Memo Card Game',
-        technologies: 'Javascript',
-        github: 'https://github.com/ijchavez/JavaScript/tree/master/MemoCard',
-        demo: 'https://ijchavez.github.io/JavaScript/MemoCard/juego.html'
-
-    },
-    {
-        id : 6,
-        image: IMG6,
-        title: 'Color Game',
-        technologies: 'Javascript',
-        github: 'https://github.com/Javascript/colorGame/index.html',
-        demo: 'https://ijchavez.github.io/JavaScript/colorGame/index.html'
-
-    },
-    {
-        id : 7,
-        image: IMG7,
-        title: 'Metric Converter API',
-        technologies: 'Javascript',
-        github: 'https://github.com/ijchavez/metriconverter',
-        demo: 'https://ijchavez.github.io/metriconverter/views/index.html'
-
-    }
-
-]
+const data = [
+  {
+    id: 13,
+    image: IMG12,
+    title: "goEvent App Fullstack app",
+    technologies:
+      "React, NextJS, Clerk, Stripe, Uploadthing, FE deployed on Vercel, BE deployed on render",
+    github: "https://github.com/ijchavez/goevent_platform",
+    demo: "https://goevent-platform.vercel.app/",
+    gitlab: "https://gitlab.com/ijchavez/goevent_platform",
+    demoapi: "https://gitlab.com/ijchavez/events-app-be",
+  },
+  {
+    id: 8,
+    image: IMG11,
+    title: "Banking App",
+    technologies: "React deployed on Vercel",
+    github: "https://gitlab.com/ijchavez/bank_modern_app",
+    demo: "https://bank-modern-app-zeta.vercel.app/",
+  },
+  {
+    id: 11,
+    image: IMG8,
+    title: "E-Commerce",
+    technologies: "NextJS, React, Sanity, Stripe deployed on Vercel",
+    github: "https://github.com/ijchavez/ecommerce-sanity-stripe-jsm",
+    demo: "https://ecommerce-sanity-stripe-jsm.vercel.app/",
+  },
+  {
+    id: 1,
+    image: IMG1,
+    title: "Notes APP",
+    technologies:
+      "NodeJS, MongoDB, bryptJS, connect-flash, express, express-handlebars, mongoose, passport deployed on Fly.io",
+    github: "https://github.com/ijchavez/notes-app",
+    demo: "https://new-notes-app.fly.dev/",
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: "Airport Flight Information",
+    technologies: "React - Axios deployed on Vercel",
+    github: "https://gitlab.com/ijchavez/flight-information",
+    demo: "https://flight-information.vercel.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Budget",
+    technologies: "Javascript",
+    github:
+      "https://github.com/ijchavez/JavaScript/tree/master/Leccion21_AplicacionPresupuesto",
+    demo: "https://ijchavez.github.io/JavaScript/Leccion21_AplicacionPresupuesto/index.html",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "To-Do List",
+    technologies: "Javascript",
+    github: "https://ijchavez.github.io/JavaScript/ToDoList/index.html",
+    demo: "https://ijchavez.github.io/JavaScript/ToDoList/index.html",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Product API",
+    technologies: "NodeJS, ExpressJS deployed on Render",
+    github: "https://gitlab.com/ijchavez/nodejs",
+    demo: "https://productsapi-n5p2.onrender.com/api/v1/products",
+  },
+  {
+    id: 10,
+    image: IMG10,
+    title: "Product Catalog",
+    technologies:
+      "React - Axios, consuming data from NodeJS API, deployed on Vercel",
+    github: "https://gitlab.com/ijchavez/productsapi",
+    demo: "https://productsapi-six.vercel.app/",
+  },
+  {
+    id: 12,
+    image: IMG10,
+    title: "Product Catalog",
+    technologies:
+      "AngularTS, consuming data from NodeJS API, deployed on Vercel",
+    github: "https://bitbucket.org/LucasHood/angularfinalutn/",
+    demo: "https://angularfinalutn.vercel.app/",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Memo Card Game",
+    technologies: "Javascript",
+    github: "https://github.com/ijchavez/JavaScript/tree/master/MemoCard",
+    demo: "https://ijchavez.github.io/JavaScript/MemoCard/juego.html",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Color Game",
+    technologies: "Javascript",
+    github: "https://github.com/Javascript/colorGame/index.html",
+    demo: "https://ijchavez.github.io/JavaScript/colorGame/index.html",
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: "Metric Converter API",
+    technologies: "Javascript",
+    github: "https://github.com/ijchavez/metriconverter",
+    demo: "https://ijchavez.github.io/metriconverter/views/index.html",
+  },
+];
 
 export default data;
